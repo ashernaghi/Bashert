f65502ea7bbfe6a39d1359e76aae3d8c2ea23f32
Yair <yairshop@gmail.com>
2018-09-04 21:22:23 -0400

    Xcode project created

---
+++ Bashert/Bashert/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Bashert
+//
+//  Created by Yair Fax on 9/4/18.
+//  Copyright © 2018 Yair Fax. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
